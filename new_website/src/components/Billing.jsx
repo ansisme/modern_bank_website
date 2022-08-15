8aebb2efb860fa82a929e9158766be43be798323
Anshul <anshulsoni2003@gmail.com>
2022-08-15 15:21:04 +0530

    Billing changes

--- new_website/src/components/Billing.jsx
+++ new_website/src/components/Billing.jsx
@@ -16,7 +16,7 @@ const Billing = () => {
         East control your <br className="sm:block hidden"/>billing & invoincing.
         </h1>
         <p className = {`${styles.paragraph}  max-w-[470px] mt-5`}>
-          njdfvhdxhuldxv drrudg rdugfhdugrfg rfogfrdhg drguhdghdr grdiu grdhgrdgudrghd ddgrgrd grdfygy rdgrd grdgrfhu
+        sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text 
         </p>
         <div className= {`flex flex-row flex-wrap sm:mt-10 mt-6`}>
           <img src={apple} alt="apple" className={`w-[120px] h-[40px] contain-contain cursor-pointer mr-5`}/>
